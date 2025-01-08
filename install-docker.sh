6c982db9d3a938e172aafa0b153ab0398506da5e
Mike Nolan <tesses@tesses.net>
2025-01-08 12:51:29 -0600

    Change to onedev

--- install-docker.sh
+++ install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash ./script.sh
 crossvm crosslang_shell_archive_maker/bin/crosslang_shell_archive_maker-1.0.0.0-prod.crvm install
-
+ls Tesses.CrossLang.WebSite/bin/
 cp Tesses.CrossLang.ShellPackage-1.0.0.0-prod.crvm /root 
 cp Tesses.CrossLang.PackageServer/bin/Tesses.CrossLang.PackageServer-1.0.0.0-prod.crvm /root
 cp Tesses.CrossLang.WebSite/bin/Tesses.CrossLang.WebSite-1.0.0.0-prod.crvm /root
