6a8c8f38ffc11b840bc692c44baba71fd91827a1
Mike Nolan <tesses@tesses.net>
2025-07-24 14:43:16 -0500

    Add reference

--- Tesses.CrossLang.Documentation/create_doc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir docs
-crosslang run > docs/Runtime.md
-cd doc_projs/RuntimeObjects
-crosslang build
-cd ../..
-crosslang run doc_projs/RuntimeObjects/bin/*.crvm > docs/RuntimeObjects.md
